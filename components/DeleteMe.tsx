7a991fb39c373c66ba722319575ef776d784c3c7
unknown <vinceoldmark@gmail.com>
2021-09-11 16:50:02 +0100

    first commit

--- components/DeleteMe.tsx
+++ components/DeleteMe.tsx
@@ -1,27 +1,22 @@
+import React, { ReactElement } from 'react';
 
-import { useRouter } from 'next/router';
-
-export default function DeleteMe() {
-  const router = useRouter();
+import Link from 'next/link'
+export default function DeleteMe(): ReactElement  {
+ 
   return (
     <main id="delete-me-wrapper">
       <section id="hero">
         <h1>Event Sold Out!? </h1>
         <p>Here is your last chance</p>
-        <button
-        onClick={() =>
-          router.push(`/events/romhack2021-tickets-151680104275`)
-        }
-   
-   
-      >
-        Search
-      </button>
+        <Link href="/events/romhack2021-tickets-151680104275">
+          <a>Search</a>
+        </Link>
+     
       </section>
       
         {/* FOOTER */}
         <footer>
-          <p>© 2021 Steve Frenzel</p>
+          <p>© 2021 Vino Bino</p>
         </footer>
       </main>
     
