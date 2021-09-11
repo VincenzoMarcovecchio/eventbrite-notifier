b1fd2e5078278be2f1791fc6400e471db801377a
unknown <vinceoldmark@gmail.com>
2021-09-11 16:26:58 +0100

    first commit

---
+++ pages/events/[slug].tsx
@@ -0,0 +1,54 @@
+import React, { ReactElement } from 'react';
+import cheerio from "cheerio";
+import axios from "axios"
+import { useRouter } from 'next/router';
+// Delete this if runtime JavaScript is needed:
+export const config = {
+  unstable_runtimeJS: false,
+};
+
+interface Props {
+  title: string;
+  lastScraped?: string;
+ 
+}
+
+export default function Home({title,lastScraped}: Props): ReactElement  {
+ const router = useRouter();
+ console.log(title)
+
+ const refreshData = () => {
+   router.replace(router.asPath);
+  
+ };
+
+ React.useEffect(() => {
+  const interval = setInterval(() => {
+    refreshData();
+  }, 9000);
+  return () => clearInterval(interval);
+
+}, []);
+
+  return (
+    <>
+     <h1>Status: {title} 😞</h1>
+     <h1>Last scraped: {lastScraped} (every 9 seconds)</h1>
+     <h3>Wanna get notified? well make a function for ya</h3>
+    </>
+  );
+}
+
+export async function getServerSideProps(pageContext) {
+  const pageNumber = pageContext.query.slug;
+  const { data } = await axios.get(
+    `https://www.eventbrite.it/e/${pageNumber}`
+  );
+  const $ = cheerio.load(data);
+  const title = $(".listing-panel-info__sm-text-center").html();
+  const lastScraped = new Date().toISOString();
+  return {
+    props: { title, lastScraped }
+   
+  };
+}
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,17 +1,19 @@
+import { useEffect, useState } from 'react';
 import Meta from '@utils/Meta';
 import DeleteMe from '@components/DeleteMe';
-
 // Delete this if runtime JavaScript is needed:
 export const config = {
   unstable_runtimeJS: false,
 };
 
 export default function Home() {
+  
+
   return (
     <>
-      <Meta />
-      <a href="#main">Skip to main content</a>
-      <DeleteMe />
+      <Meta /><DeleteMe />
+     
+      
     </>
   );
 }
